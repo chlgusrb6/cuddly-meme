2799e7f95b6da2ce258ff263c608443b9b92d2f6
chlgusrb6 <chlgusrb6@naver.com>
2018-11-16 08:56:35 +0900

    신규커밋

--- webStudy01/WebContent/WEB-INF/views/fileBrowser.jsp
+++ webStudy01/WebContent/WEB-INF/views/fileBrowser.jsp
@@ -1,3 +1,7 @@
+<%@page import="org.apache.commons.lang3.StringUtils"%>
+<%@page import="java.util.Map.Entry"%>
+<%@page import="java.util.Map"%>
+<%@page import="java.io.File"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -5,9 +9,44 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+   <%
+      Map<String, String>  files =  (Map<String,String>)request.getAttribute("folders");
+      
+      File[] liFiles = (File[])request.getAttribute("liFiles");
+   %>
+<script type="text/javascript">
+   function showList(command){
+       var form=document.listForm;
+       form.command.value = command;
+      sd = command;
+       form.submit();
+    }
+</script>
 </head>
 <body>
 <!-- 파일들의 목록이 directory라면  -->
 <!-- parameter 설계 해서 2번을 클릭했다면 2번을 클릭햇다라는 정보를 보냄 -->
+   <form name="listForm">
+   <%
+      
+      String format = "<ul onclick=\"javascript:showList('%s')\">%s";   
+      String liFormat = "<li>%s</li>";
+      for(Entry<String,String> entry : files.entrySet()){
+         out.println(String.format(format,  entry.getKey(), entry.getKey())); 
+
+         if(liFiles != null){
+            for(File li : liFiles){
+               if(li.getParentFile().getName().equals(entry.getKey())){
+                  out.println(String.format(liFormat,li.getName()));
+               }
+            }
+         }
+         out.println("</ul>");
+      }
+      
+      
+   %>
+      <input type="hidden" name="command" value = "">
+   </form>
 </body>
 </html>
--- webStudy01/src/kr/or/ddit/web/ServerFileBrowser.java
+++ webStudy01/src/kr/or/ddit/web/ServerFileBrowser.java
@@ -1,6 +1,13 @@
 package kr.or.ddit.web;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,14 +16,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
+
 @WebServlet("/fileBrowser.do")
 public class ServerFileBrowser extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
-		String view = "/WEB-INF/views/fileBrowser.jsp";
-		RequestDispatcher rd  = req.getRequestDispatcher(view);
-		rd.forward(req, resp);
-	}
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   
+      File webContent = new File(getServletContext().getRealPath("/"));
+      File[] files = webContent.listFiles();
+
+      Map<String, String> fileList = new LinkedHashMap<>();
+      
+      for (File file : files) {
+         if (file.isDirectory()) {
+            fileList.put(file.getName(), file.getAbsolutePath());
+         }
+      }
+         
+      req.setAttribute("folders", fileList);
+      
+      String command =  req.getParameter("command");
+      
+      if(command!=null) {
+         File liList = new File(fileList.get(command));
+         File liFiles[] = null;
+         liFiles = liList.listFiles();
+         req.setAttribute("liFiles", liFiles);
+      }
+      
+      String view = "/WEB-INF/views/fileBrowser.jsp";
+      RequestDispatcher rd  = req.getRequestDispatcher(view);
+      rd.forward(req, resp);
+   }
 }
